6ccd5dc5a58246a502999158b75d40312c23ba29
isaacwach <isaac.ndirangu@student.moringaschool.com>
2022-04-01 23:47:21 +0300

    created an updateuserform

--- social/forms.py
+++ social/forms.py
@@ -15,3 +15,10 @@ class NewUserForm(UserCreationForm):
 		if commit:
 			user.save()
 		return user
+
+class UpdateUserForm(forms.ModelForm):
+    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+
+    class Meta:
+        model = User
+        fields = ('username', 'email')
