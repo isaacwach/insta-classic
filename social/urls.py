0856e055945ffba8aa55fa825033e44b1766fb57
isaacwach <isaac.ndirangu@student.moringaschool.com>
2022-04-01 21:15:51 +0300

    created register_request function

--- social/urls.py
+++ social/urls.py
@@ -1,2 +1,10 @@
 from django.urls import path
 from . import views
+
+app_name = "social"   
+
+
+urlpatterns = [
+    path("", views.homepage, name="homepage"),
+    path("register", views.register_request, name="register")
+]
