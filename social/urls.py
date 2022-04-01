768285c57c3ad51fd6e32aec199bd89bf93586b8
isaacwach <isaac.ndirangu@student.moringaschool.com>
2022-04-02 00:31:19 +0300

    created paths for profile and userprofile

--- social/urls.py
+++ social/urls.py
@@ -7,5 +7,7 @@ app_name = "social"
 urlpatterns = [
     path("", views.homepage, name="homepage"),
     path("register", views.register_request, name="register"),
-    path("login", views.login_request, name="login")
+    path("login", views.login_request, name="login"),
+    path('user_profile/<username>/', views.user_profile, name='user_profile'),
+    path('profile/<username>/', views.profile, name='profile'),
 ]
