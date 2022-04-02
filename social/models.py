7edb439c2831a6e777c7ea0f4bb0cb8e88b20fe2
isaacwach <isaac.ndirangu@student.moringaschool.com>
2022-04-02 19:41:08 +0300

    added a post class in the models

--- social/models.py
+++ social/models.py
@@ -3,6 +3,36 @@ from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 from django.dispatch import receiver
 
+class Post(models.Model):
+    name = models.CharField(max_length=300, blank=True)
+    image = models.ImageField(upload_to='posts/')
+    caption = models.CharField(max_length=300, blank=True)
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='posts')
+    likes = models.ManyToManyField(User, related_name='likes', blank=True, )
+    created = models.DateTimeField(auto_now_add=True, null=True)
+
+    class Meta:
+        ordering = ["-pk"]
+
+    def get_absolute_url(self):
+        return f"/post/{self.id}"
+
+    @property
+    def get_all_comments(self):
+        return self.comments.all()
+
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()
+
+    def total_likes(self):
+        return self.likes.count()
+
+    def __str__(self):
+        return f'{self.user.name} Post'
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
     profile_picture = models.ImageField(upload_to='images/', default='default.png')
