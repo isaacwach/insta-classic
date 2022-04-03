4be451dbd4203b94b938181fb50b680b8377a54c
isaacwach <isaac.ndirangu@student.moringaschool.com>
2022-04-03 10:16:48 +0300

    performed database migrations

--- social/views.py
+++ social/views.py
@@ -23,7 +23,7 @@ def register_request(request):
 			user = form.save()
 			login(request, user)
 			messages.success(request, "Registration successful." )
-			return redirect("social:homepage")
+			return redirect("/login")
 		messages.error(request, "Unsuccessful registration. Invalid information.")
 	form = NewUserForm()
 	return render (request=request, template_name="register.html", context={"register_form":form})
